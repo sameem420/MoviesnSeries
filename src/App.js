a36a105a8c260f3755758998d725358a1e615862
Muhammad Sameem <sameemgujjar123@yahoo.com>
2023-02-09 19:10:49 +0400

    added bootstrap and initial form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import "./App.css";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Movies n Series</h1>
-    </div>
+    <Container fluid>
+      <h1 className="text-center main-heading">Movies n Series</h1>
+      <Row>
+        <Col>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Movie or Series</Form.Label>
+              <Form.Control type="text" placeholder="Enter Movie or Series name" />  
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Add
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+      <Row md={12} className="justify-content-center mx-auto">
+        <Col md>1 of 3</Col>
+        <Col md>2 of 3</Col>
+        <Col md>3 of 3</Col>
+      </Row>
+    </Container>
   );
 }
 
