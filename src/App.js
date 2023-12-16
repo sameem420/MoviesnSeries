801e4451a1cf9e4f53a3c9e782ab4c88a3e2a92f
Muhammad Sameem <sameemgujjar123@yahoo.com>
2023-12-17 03:54:20 +0400

    :heavy_check_mark: refactord the addmovie component
    moving away from components to plain html & css classes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
-import { Container } from "react-bootstrap";
 import AddMovie from "./Components/AddMovie";
 
 function App() {
   return (
-    <Container fluid>
-      <h1 className="text-center main-heading">Movies n Series</h1>
+    <div className="container-fluid">
+      <h1 className="text-center main-heading">
+        TVLog
+      </h1>
       <AddMovie />
-    </Container>
+    </div>
   );
 }
 
--- src/Components/AddMovie.js
+++ src/Components/AddMovie.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 function AddMovie() {
   const [movieRecord, setMovieRecord] = useState({
@@ -15,29 +14,29 @@ function AddMovie() {
   };
 
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Form onSubmit={(e) => addMovieRecord(e)}>
-            <Form.Group className="mb-3" controlId="seriesName">
-              <Form.Label>Series Name</Form.Label>
-              <Form.Control
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <form onSubmit={(e) => addMovieRecord(e)}>
+            <div className="form-group mb-3" controlId="seriesName">
+              <label className="form-label">Series Name</label>
+              <input className="form-control"
                 type="text"
                 placeholder="eg: Teen Wolf"
                 value={movieRecord.name}
               />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="seriesYear">
-              <Form.Label>Series Starting Year</Form.Label>
-              <Form.Control
+            </div>
+            <div className="form-group mb-3" controlId="seriesYear">
+              <label className="form-label">Series Starting Year</label>
+              <input className="form-control"
                 type="text"
                 placeholder="eg: 2005"
                 value={movieRecord.startingYear}
               />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="seriesSeason">
-              <Form.Label>Series Season</Form.Label>
-              <Form.Select
+            </div>
+            <div className="form-group mb-3" controlId="seriesSeason">
+              <label className="form-label">Series Season</label>
+              <select className="form-control"
                 aria-label="series-season"
                 value={movieRecord.season}
               >
@@ -46,20 +45,20 @@ function AddMovie() {
                 <option value="Season 3">Season 3</option>
                 <option value="Season 4">Season 4</option>
                 <option value="Season 5">Season 5</option>
-              </Form.Select>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Add Series
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-      <Row md={12} className="justify-content-center mx-auto">
-        <Col md>1 of 3</Col>
-        <Col md>2 of 3</Col>
-        <Col md>3 of 3</Col>
-      </Row>
-    </Container>
+              </select>
+            </div>
+            <button className="btn btn-success" variant="primary" type="submit">
+              Add Movie
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className=" row justify-content-center mx-auto">
+        <div className="col">1 of 3</div>
+        <div className="col">2 of 3</div>
+        <div className="col">3 of 3</div>
+      </div>
+    </div>
   );
 }
 
