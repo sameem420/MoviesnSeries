d13ebc3205aa55cbe02d4eb447fd42fec0c94d5f
Muhammad Sameem <sameemgujjar123@yahoo.com>
2023-12-19 00:22:12 +0400

    :heavy_check_mark: implemented saveSeries functionality

--- src/Components/AddSeries.js
+++ src/Components/AddSeries.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function AddSeries() {
   const [seriesRecord, setSeriesRecord] = useState({
@@ -6,6 +6,15 @@ function AddSeries() {
     startingYear: undefined,
     season: undefined,
   });
+
+  const [seriesTitle, setSeriesTitle] = useState("");
+  let seriesRef = useRef(null);
+
+  const saveSeriesinfo = () => {
+    console.log(seriesRef.current.value);
+    setSeriesTitle(seriesRef.current.value);
+    localStorage.setItem("series", seriesTitle);
+  };
 
   const addSeriesRecord = (e) => {
     e.preventDefault();
@@ -18,42 +27,43 @@ function AddSeries() {
       <div className="row">
         <form onSubmit={(e) => addSeriesRecord(e)}>
           <div className="col-md-4">
-            <div className="form-group mb-3" controlId="seriesName">
+            <div className="form-group mb-3">
               <label className="form-label">Series Name</label>
               <input
                 className="form-control"
                 type="text"
                 placeholder="eg: Teen Wolf"
-                value={seriesRecord.name}
+                ref={seriesRef}
+                onChange={saveSeriesinfo}
               />
             </div>
           </div>
           <div className="col-md-4">
-          <div className="form-group mb-3" controlId="seriesYear">
-            <label className="form-label">Series Starting Year</label>
-            <input
-              className="form-control"
-              type="text"
-              placeholder="eg: 2005"
-              value={seriesRecord.startingYear}
-            />
-          </div>
+            <div className="form-group mb-3">
+              <label className="form-label">Series Starting Year</label>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="eg: 2005"
+                value={seriesRecord.startingYear}
+              />
+            </div>
           </div>
           <div className="col-md-4">
-          <div className="form-group mb-3" controlId="seriesSeason">
-            <label className="form-label">Series Season</label>
-            <select
-              className="form-control"
-              aria-label="series-season"
-              value={seriesRecord.season}
-            >
-              <option value="Season 1">Season 1</option>
-              <option value="Season 2">Season 2</option>
-              <option value="Season 3">Season 3</option>
-              <option value="Season 4">Season 4</option>
-              <option value="Season 5">Season 5</option>
-            </select>
-          </div>
+            <div className="form-group mb-3" controlId="seriesSeason">
+              <label className="form-label">Series Season</label>
+              <select
+                className="form-control"
+                aria-label="series-season"
+                value={seriesRecord.season}
+              >
+                <option value="Season 1">Season 1</option>
+                <option value="Season 2">Season 2</option>
+                <option value="Season 3">Season 3</option>
+                <option value="Season 4">Season 4</option>
+                <option value="Season 5">Season 5</option>
+              </select>
+            </div>
           </div>
           <div class="form-check form-switch mb-3">
             <input
@@ -66,14 +76,19 @@ function AddSeries() {
             </label>
           </div>
           <button className="btn btn-primary" type="submit">
-            Add Series
+            Save
           </button>
         </form>
       </div>
       <div className="row justify-content-center mx-auto">
-        <div className="col-md">1 of 3</div>
-        <div className="col-md">2 of 3</div>
-        <div className="col-md">3 of 3</div>
+        <div className="col-md">
+          <table>
+            <tr>
+              <th>Series Name</th>
+              <td>{localStorage.getItem("series")}</td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   );
