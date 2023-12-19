ba2db15e576e1e16d53042a12e2611acd313ef76
Muhammad Sameem <sameemgujjar123@yahoo.com>
2023-12-19 23:58:45 +0400

    :heavy_check_mark: implemented functionality for series record

--- src/Components/AddSeries.js
+++ src/Components/AddSeries.js
@@ -1,31 +1,31 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 
 function AddSeries() {
-  const [seriesRecord, setSeriesRecord] = useState({
+  const [seriesInfo, setSeriesInfo] = useState({
     name: undefined,
     startingYear: undefined,
     season: undefined,
+    watched: false,
   });
 
-  const [seriesTitle, setSeriesTitle] = useState("");
-  let seriesRef = useRef(null);
+  const [seriesRecords, setSeriesRecords] = useState([]);
 
-  const saveSeriesinfo = () => {
-    console.log(seriesRef.current.value);
-    setSeriesTitle(seriesRef.current.value);
-    localStorage.setItem("series", seriesTitle);
-  };
-
-  const addSeriesRecord = (e) => {
+  const saveSeriesInfo = (e) => {
     e.preventDefault();
-    console.log("Function called!");
-    const seriesData = [...seriesRecord, {}];
+    setSeriesRecords((prevRecords) => [...prevRecords, seriesInfo]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("seriesRecords", JSON.stringify(seriesRecords));
+    if (localStorage.getItem("seriesRecords").length !== 0) {
+      setSeriesRecords(localStorage.getItem("seriesRecords"));
+    }
+  }, [seriesRecords]);
 
   return (
     <div className="container">
       <div className="row">
-        <form onSubmit={(e) => addSeriesRecord(e)}>
+        <form>
           <div className="col-md-4">
             <div className="form-group mb-3">
               <label className="form-label">Series Name</label>
@@ -33,8 +33,10 @@ function AddSeries() {
                 className="form-control"
                 type="text"
                 placeholder="eg: Teen Wolf"
-                ref={seriesRef}
-                onChange={saveSeriesinfo}
+                value={seriesInfo.name}
+                onChange={(e) =>
+                  setSeriesInfo({ ...seriesInfo, name: e.target.value })
+                }
               />
             </div>
           </div>
@@ -45,17 +47,23 @@ function AddSeries() {
                 className="form-control"
                 type="text"
                 placeholder="eg: 2005"
-                value={seriesRecord.startingYear}
+                value={seriesInfo.startingYear}
+                onChange={(e) =>
+                  setSeriesInfo({ ...seriesInfo, startingYear: e.target.value })
+                }
               />
             </div>
           </div>
           <div className="col-md-4">
-            <div className="form-group mb-3" controlId="seriesSeason">
+            <div className="form-group mb-3">
               <label className="form-label">Series Season</label>
               <select
                 className="form-control"
                 aria-label="series-season"
-                value={seriesRecord.season}
+                value={seriesInfo.season}
+                onChange={(e) =>
+                  setSeriesInfo({ ...seriesInfo, season: e.target.value })
+                }
               >
                 <option value="Season 1">Season 1</option>
                 <option value="Season 2">Season 2</option>
@@ -65,17 +73,28 @@ function AddSeries() {
               </select>
             </div>
           </div>
-          <div class="form-check form-switch mb-3">
+          <div className="form-check form-switch mb-3">
             <input
-              class="form-check-input"
+              className="form-check-input"
               type="checkbox"
               id="flexSwitchCheckReverse"
+              checked={seriesInfo.watched}
+              onChange={(e) =>
+                setSeriesInfo({ ...seriesInfo, watched: e.target.checked })
+              }
             />
-            <label class="form-check-label" for="flexSwitchCheckReverse">
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckReverse"
+            >
               Watched (yes or no?)
             </label>
           </div>
-          <button className="btn btn-primary" type="submit">
+          <button
+            className="btn btn-primary"
+            type="submit"
+            onClick={saveSeriesInfo}
+          >
             Save
           </button>
         </form>
@@ -83,10 +102,16 @@ function AddSeries() {
       <div className="row justify-content-center mx-auto">
         <div className="col-md">
           <table>
-            <tr>
-              <th>Series Name</th>
-              <td>{localStorage.getItem("series")}</td>
-            </tr>
+            <thead>
+              <tr>
+                <th>Series Data</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{localStorage.getItem("seriesRecords")}</td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
